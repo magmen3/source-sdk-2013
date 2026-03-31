c44b54f945a5a23081bce29b6963db9612029d64
Mannytko <78595463+magmen3@users.noreply.github.com>
2026-03-31 10:33:03 +0300

    майнеры трояны

--- sp/src/game/client/ragdoll.cpp
+++ sp/src/game/client/ragdoll.cpp
@@ -272,7 +272,7 @@ void CRagdoll::PhysForceRagdollToSleep()
 }
 
 #define RAGDOLL_SLEEP_TOLERANCE	1.0f
-static ConVar ragdoll_sleepaftertime( "ragdoll_sleepaftertime", "5.0f", 0, "After this many seconds of being basically stationary, the ragdoll will go to sleep." );
+static ConVar ragdoll_sleepaftertime( "ragdoll_sleepaftertime", "15.0f", 0, "After this many seconds of being basically stationary, the ragdoll will go to sleep." );
 
 void CRagdoll::CheckSettleStationaryRagdoll()
 {
